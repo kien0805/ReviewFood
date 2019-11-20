2e3a16fd42d608753c51fc3f1b90a87c512a41bd
namnguyenktxd <namnguyenktxd@gmail.com>
2019-11-21 01:00:21 +0700

    update code

--- index.js
+++ index.js
@@ -6,20 +6,50 @@ var dataReceive =
         .then(json => { data = json })
 
 function showData(id) {
+    console.log(id)
     const idFood = document.getElementById("food_pick")
     const food = document.getElementById("food")
     idFood.style.display = "none"
-    const html = `
-    <h1><b class="darkblue">Đồ ăn tối</b></h1>
+    data.map(item => {
+        if (id === item.type) {
+            const html = `
+    <h1><b class="darkblue">${item.type}</b></h1>
 
-    <div class="line">
+    <div id="${item.name}" onclick="showDetailFood(this.id)" class="line" >
         <div class="container">
             <img src="https://media.cooky.vn/recipe/g6/50772/s800x500/cooky-recipe-637093679153815286.JPG"
                 alt="Thịt bò xào dưa cải chua" class="image">
-            <div class="overlay">${data.name}</div>
+            <div class="overlay" >${item.name}</div>
         </div>
     </div>
     `
-    food.insertAdjacentHTML("beforeend", html)
-    food.style.display = "block"
+            food.insertAdjacentHTML("beforeend", html)
+            food.style.display = "block"
+
+        }
+    })
+}
+
+
+function showDetailFood(id) {
+    const idElement = document.getElementById("line")
+    document.getElementById('food').style.display = "none"
+    data.map(item => {
+        const ingrediente = item.ingrediente
+        const steps = item.steps
+        if (item.name === id) {
+            const html = `
+                <h1>${id}</h1>
+                <img src=${item.thumbnail} />
+                <p>Nguyên Liệu:</p>
+                ${ingrediente.map((value, index) => { return `<p>${index} ${value} </p>` })}
+                <p>Các bước thực hiện: </p>
+                ${steps.map((value, index) => { return `<p>${index}  ${value}  </p>` })}
+            `
+            idElement.insertAdjacentHTML("beforeend", html)
+            idElement.style.display = "block"
+        }
+    })
+
+
 }
