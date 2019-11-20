aad5fd71135206f5aa124a33ee1bbf09db838254
namnguyenktxd <namnguyenktxd@gmail.com>
2019-11-20 19:54:12 +0700

    a Nam push

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
 var data;
 
 var dataReceive =
-    fetch('https://arcane-ridge-39561.herokuapp.com/api/food')
+    fetch('https://dev-c4t-test.herokuapp.com/api/food')
         .then(response => { return response.json() })
         .then(json => { data = json })
+
+function showData(id) {
+    const idFood = document.getElementById("food_pick")
+    const food = document.getElementById("food")
+    idFood.style.display = "none"
+    const html = `
+    <h1><b class="darkblue">Đồ ăn tối</b></h1>
+
+    <div class="line">
+        <div class="container">
+            <img src="https://media.cooky.vn/recipe/g6/50772/s800x500/cooky-recipe-637093679153815286.JPG"
+                alt="Thịt bò xào dưa cải chua" class="image">
+            <div class="overlay">${data.name}</div>
+        </div>
+    </div>
+    `
+    food.insertAdjacentHTML("beforeend", html)
+    food.style.display = "block"
+}
